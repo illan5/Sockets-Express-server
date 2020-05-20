9529ddf3ee3a8de5220600c4f17532d4da4ff607
Jose Illan <jose_illan@hotmail.com>
2020-05-20 20:12:34 +0200

    Identify useres connected

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -19,5 +19,17 @@ export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
         io.emit('mensaje-nuevo', payload );
 
     });
+}
 
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on('configurar-usuario', ( payload: { nombre: string }, callback: Function ) => {
+
+        console.log('Configurando usuario', payload.nombre );
+        callback({
+            ok: true,
+            mensaje: `Usuario ${ payload.nombre }, configurado`
+        });
+    });
 }
